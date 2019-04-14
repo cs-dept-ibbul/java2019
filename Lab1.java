3b887a5ebaf16b4c3106342084689eec048454c2
Phoenixkheeng <pheonixkheeng@gmail.com>
2019-04-14 05:29:34 -0700

    La1,U15fnscsc014

---
+++ Lab1.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Lab1 here.
+ * 
+ * @author (u15/fns/csc014) 
+ */
+public class Lab1
+{
+  public static void main(String[]args){  
+ 
+ System.out.println("*"+"\n"+"*"+"\n"+"*"+"\n"+"*"+"\n"+"*"+"\n");
+ //System.out.print("\t*"+"\n"+"    "+"*"+"       "+"*"+"\n"+"  "+"*"+"\t*"+"     "+"*"+"\n"+"  "+"*"+" "+"\t*"+"     "+"*"+"\n\n");
+ System.out.print("*"+"  "+"*"+"\n"+"*"+"  "+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"\n\n");
+ System.out.print("*"+"  "+"*"+"\n"+"*"+"  "+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"  "+"*"+"\n"+"*"+"  "+"*"+"\n\n");
+ System.out.print("*"+"\t*"+"\n"+"*"+"\t*"+"\n"+"*"+"\t*"+"\n"+"  "+"*"+"   "+"*"+"\n\n");
+ System.out.println("EXERCISE 02");
+ System.out.println("\t1"+"\t2"+"\t3"+"\t4"+"\n"+"1"+"\t1"+"\t2"+"\t3"+"\t4"+"\n"+"2"+"\t2"+"\t4"+"\t6"+"\t16"+"\n"+"3"+"\t3"+"\t6"+"\t9"+"\t12"+"\n"+"4"+"\t4"+"\t8"+"\t12"+"\t16"+"\n");
+}
+}
