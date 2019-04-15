6f853170c05548b40882fd54e90035a7ff746680
mamud19 <mamudalkali@gmail.com>
2019-04-15 13:08:22 +0100

    Lab1,U15FNSCSC061

---
+++ Lab1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author hp
+ */
+public class Lab1 {
+
+  
+    public static void main(String[] args) {
+       System.out.println("*\t*\n*\t*\n*\t*\n*\t*\n*\t*");
+		System.out.println("\n\n\n*\t*\n\n*\t*\t*\n\n*\t*\n\n*\t*\t*\n\n*\t*");
+               System.out.println("\n\n\n*\t*\n\n*\t*\t*\n\n*\t*\n\n*\t*\t*\n\n*\t*");
+		System.out.println("\n\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n    * \t   *");
+    }
+    
+}
