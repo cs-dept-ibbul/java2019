572f4acb92b118d04396b4cfeadbdc87a1e6835e
sheshi325 <sheshi325@gmail.com>
2019-04-16 04:41:37 -0700

    Lab1-2

---
+++ java/patternDisplay.java
@@ -0,0 +1,12 @@
+package practicalClass;
+
+//program to display a given pattern
+//U14/FNS/CSC/028
+public class patternDisplay {
+	public static void main(String[] args) {
+		System.out.println("\t*\n\n   *\t     *\n\n*\t*\t*\n\n*\t\t*");
+		System.out.println("\n\n\n*\t*\n\n*\t*\t*\n\n*\t*\n\n*\t*\t*\n\n*\t*");
+		System.out.println("\n\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n    * \t   *");
+	}
+
+}
