6b548e93032b56e9caeb7d98fb384bfc43e777d2
jessejohnz <48954601+jessejohnz@users.noreply.github.com>
2019-04-18 15:19:32 +0100

    Lab01, U14/FNS/CSC/082

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Pattern here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pattern
+{
+    // Program to display pattern
+     public static void main (String args[]){ 
+        System.out.println("    *\n *     *\n*   *   *\n*       *\n");
+        System.out.println("*  *\n*  *  *\n*  *\n*  *  *\n*  *\n");
+        System.out.println("*   *\n*   *\n*   *\n * * \n");
+    }
+    
+    
+}
---
+++ Table.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Table here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Table
+{
+   
+     public static void main (String args[]){ 
+       System.out.println("      1      2      3      4\n");
+       System.out.println("1     1      2      3      4\n");
+       System.out.println("2     2      4      6      16\n");
+       System.out.println("3     3      6      9      12\n");
+       System.out.println("4     4      8      12     16\n");
+    }
+}
