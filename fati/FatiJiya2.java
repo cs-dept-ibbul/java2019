1deb91e1fb5459fabcd43da41bb96fe1e27130d4
fatimjiya <fatijiya1@gmail.com>
2019-04-26 00:54:59 -0700

    lab1-2,u15/fns/csc/091

---
+++ fati/FatiJiya2.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class FatiJiya2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FatiJiya2
+{
+   public static void main(String[] args)
+   {
+       System.out.println("      1     2     3     4\n1     1     2     3     4\n2     2     4     6     16\n3     3     6     9     12\n4     4     8     12    16");
+       System.out.println("Workdone by:U15/FNS/CSC/091");
+    }
+}
