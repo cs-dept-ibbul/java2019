ea7041847b769f7b5a70f678a4e42615261d5e5b
sman2019 <mr911sman@gmail.com>
2019-04-29 03:56:06 -0700

    LAB1-2 U14/FNS/CSC/112

---
+++ Lab_1-2/project 1/Project1.java
@@ -0,0 +1,17 @@
+public class Project1
+{               
+    public static void main (String []args){
+        System.out.println("IBRAHIM HADI \"To passs , you must work hard!\".\n");
+        System.out.println("\t* "+"\n\n    *\t    *"+"\n\n*\t*\t*\n\n*\t\t*");
+        System.out.println("");
+        System.out.println("*  *\n*  *  *\n*  *\n*  *  *\n*  *");
+        System.out.println("");
+        System.out.println("*    *\n*    *\n*    *\n *  *");
+        System.out.println("MATRIC NUMBER::: U15/FNS/CSC/079");
+
+
+    }
+
+}   
+
+
