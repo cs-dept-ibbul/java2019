2eed6c8dca5840d48df3cc361766bceb3f287341
umarkuta <umargarba8585@gmail.com>
2019-05-04 18:44:17 +0100

    lab4, lab5 and lab6, u15fnscsc029

---
+++ Lab4/BankAccount.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class BankAccount here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ 
+*/
+
+public class BankAccount
+{
+   
+ // instance variables - replace the example below with your own
+   
+ private int accountNumber;
+   
+ private String customerName;
+  
+  private double balance;
+
+    /**Constructor for objects of class BankAccount
+     */
+   
+ public BankAccount(int acctNumber, String custName, double bal)
+    {
+       
+ accountNumber=acctNumber;
+    
+    customerName=custName;
+      
+  balance=bal;
+   
+ }
+
+    
+//method deposit
+   
+ public void deposit(double deposit)
+    {
+      
+  balance+=deposit;
+   
+ }
+    //method withdraw
+   
+ public double withdraw(double withdraw)
+    {
+      
+  balance-=withdraw;
+     
+   return balance;
+  
+  }
+  
+  public double getBalance()
+    {
+     
+   return balance;
+    
+}
+
+}
