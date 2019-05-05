34cb0c1b90e21ce7cf59de81bc0c4520beeabefa
_number419 <yousufabbakar@gmail.com>
2019-05-05 15:31:38 +0100

    U15FNSCSC081

---
+++ Lab4/BoxDemo.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class BoxDemo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BoxDemo
+{
+    
+    public static void main(String[] args){
+            // Create two Box obj ects
+    Box box1 = new Box(20.0, 10.0, 15.0);
+    Box box2 = new Box(6.0, 4.0, 2.0);
+    
+    double volume, area;
+    // Get and display volume and surface area of box1
+    volume = box1.volume();
+    area = box1.surfaceArea();
+    System.out.println("The volume of box1 is " + volume + " cubic cm");
+    System.out.println("The surface area of box1 is "+area+" square cm\n");
+    // Get and display volume of surface area box2
+    System.out.println("The volume of box2 is " + box2.volume() + " cubic cm");
+    System.out.println("The surface area of box2 is "+area+" square cm\n");
+    }
+}
---
+++ Lab4/Circle.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Circle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Circle
+{
+    private static int numberOfCircles = 0;//a static variable
+    private double radius; //instance variables
+    public Circle(double circleRadius) {
+    numberOfCircles++;
+    radius = circleRadius;
+    }
+    public double area() {
+    return Math. PI * radius * radius;
+    }
+    public double circumference() {
+    return 2.0 * Math.PI * radius;
+    }
+    
+    public static int getNumberOfCircles() {
+    return numberOfCircles;
+    }
+}
