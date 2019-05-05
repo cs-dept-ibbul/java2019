b348c4bdfc2abb38f5598d8d3acf651a7b308223
Abdulidris <48857318+Abdulidris@users.noreply.github.com>
2019-05-05 23:23:59 +0100

    Lab6 u16/fns/csc/2087

---
+++ TestAuthor.java
@@ -0,0 +1,20 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//MATRIC NUMBER:U16/FNS/CSC/2087
+//NAME:Abdullahi idris;
+package TestAuthor;
+
+public class TestAuthor {
+   public static void main(String[] args) {
+      Author auth = new Author("Alkali Mamud "Mamudalkakli@yahoo.com", 'm');
+      System.out.println(auth); 
+      auth.setEmail("mamudalkali@yahoo.com");
+      System.out.println(auth);
+      System.out.println("name is: " + auth.getName());
+      System.out.println("gender is: " + auth.getGender());
+      System.out.println("email is: " + auth.getEmail());
+   }
+}
