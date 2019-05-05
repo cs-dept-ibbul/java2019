45985783d989ada4737535e6c9c4fbc6dba91b30
Abdulidris <48857318+Abdulidris@users.noreply.github.com>
2019-05-05 23:27:12 +0100

    Lab6 u16/fns/csc/2087

---
+++ TestEmployee2.java
@@ -0,0 +1,37 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//MATRIC NUMBER:U16/FNS/CSC/2087;
+//NAME:Abdullahi idris;
+package TestEmployee2;
+
+import java.util.Scanner;
+public class TestEmployee2
+{
+    public static void main(String[ ] args) {
+        Scanner input = new Scanner(System. in);
+        int number;
+        String name;
+        double salary;
+        System.out.print("Enter Name for Employee 1: ");
+        name = input.nextLine();
+        System.out.print("Enter ID Number for Employee 1: ");
+        number = input.nextInt();
+        System.out.print("Enter Salary for Employee 1: ");
+        salary = input.nextDouble();
+        Employee2 emp1 = new Employee2(number, name, salary);
+        input.nextLine();
+        System.out.print("\nEnter Name for Employee 2: ");
+        name = input.nextLine();
+        System.out.print("Enter ID Number for Employee 2: ");
+        number = input.nextInt();
+        Employee2 emp2 = new Employee2(number, name);
+        emp2.setSalary(emp1.getSalary());
+        emp1.deductions(50);
+        emp2.deductions(60, 40);
+        emp1.printDetails();
+        emp2.printDetails();
+    }
+}
