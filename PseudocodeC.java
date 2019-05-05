dda5ec846b54162041edacc3c23c30419e802c12
Abdulidris <48857318+Abdulidris@users.noreply.github.com>
2019-05-05 23:10:17 +0100

    Lab5 u16/fns/csc/2087

---
+++ PseudocodeC.java
@@ -0,0 +1,33 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//MATRIC NUMBER:U16/FNS/CSC/2087;
+//NAME:Abdullahi idris;
+package PseudocodeC;
+
+import java.util.Scanner;
+public class PseudocodeC
+{
+public static void main(String[]arg){
+int passes=0;
+int failure=0;
+int student=1;
+int result;
+Scanner input = new Scanner(System.in);
+while(student<=10){
+System.out.println("Enter result,1=pass,0=fail:");
+result=input.nextInt();
+if(result==1)
+passes=passes+1;
+else
+failure=failure+1;
+student=student+1;
+}
+System.out.println("Passed: "+passes);
+System.out.println("Failed: "+failure);
+if(passes>8)
+System.out.println("Execellent to instructor");
+}   
+}
