7c367bdb9196a3d432e0df62fb6dc1cdb978065e
Abdulidris <48857318+Abdulidris@users.noreply.github.com>
2019-05-05 19:47:15 +0100

    Lab4 u16/fns/csc/2087

---
+++ BankAccount.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankaccount;
+
+/**
+ *
+ * @author hp
+ */
+//MATRIC NUMBER:U15/FNS/CSC/061
+//NAME:ALKALI MAMUD
+public class BankAccount {
+private int accountNumber;
+private String customerName;
+private double balance;
+    public BankAccount (int acctNumber, String custName, double bal){
+        accountNumber=acctNumber;
+        customerName=custName;
+        balance=bal;
+    }
+    public void deposit(double deposit){
+        balance+=deposit;
+    }
+    public double withdraw(double withdraw){
+        balance-=withdraw;
+        return balance;
+    }
+    public double getBalance(){
+        return balance;
+    }
+          
+        
+    }
+    
+
---
+++ BankAccountDemo.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankaccountdemo;
+
+/**
+ *
+ * @author hp
+ */
+//MATRIC NUMBER:U15/FNS/CSC/061
+//NAME:ALKALI MAMUD
+import java.util.Scanner;
+public class BankAccountDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        Scanner bank= new Scanner(System.in);
+        int accountNumber;
+        double balance,withdraw,deposit;
+        String accountName;
+        
+        System.out.print("\nEnter Account Number: ");
+        accountNumber=bank.nextInt();
+        bank.nextLine();
+        System.out.print("Enter Customer name: ");
+        accountName=bank.nextLine();
+        System.out.print("Enter initial balance: ");
+        balance=bank.nextDouble();
+        bank.nextLine();
+        BankAccount account= new BankAccount(accountNumber,accountName,balance);
+       // BankAccount account= new BankAccount(accountNumber,accountName,balance);
+       
+       
+        System.out.print("\nEnter amount to deposit: ");
+        deposit=bank.nextDouble();
+        account.deposit(deposit);
+        System.out.print("New balance is: "+account.getBalance());
+        System.out.print("\n\nEnter amount to withdraw: ");
+        withdraw=bank.nextDouble();
+        account.withdraw(withdraw);
+        System.out.print("New balance is: "+account.getBalance());
+    }
+}
+
+    
+   
