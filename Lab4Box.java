e20db4f0d2b3b30b2d2d263ef945fe889e6d34f3
adamumohammadbako <49240725+adamumohammadbako@users.noreply.github.com>
2019-05-06 09:31:45 +0100

    Lab4 U15/fns/csc/063

---
+++ Lab4Box.java
@@ -0,0 +1,21 @@
+﻿public class Box
+ {
+     private double length, width, height;
+
+      public Box(double boxLength, double boxWidth, double boxHeight)
+     {
+         length = boxLength;
+         width = boxWidth;
+         height = boxHeight;
+     }
+
+      public double volume()
+     {
+         return length * width * height;
+     }
+
+      public double surfaceArea()
+     {
+         return 2 * (length*width + length*height + width*height);
+     }
+ }
